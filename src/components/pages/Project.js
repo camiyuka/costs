ddffcd38cb2225c809b005464cd3b1f186f52e41
camilayatabe <camila.yatabe@gmail.com>
2025-03-07 23:15:23 -0300

    feat: btn and show project data

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -1,31 +1,73 @@
-import style from './Project.module.css';
-import { useParams } from 'react-router-dom';
-import { useState, useEffect } from 'react';
+import style from "./Project.module.css";
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+
+import Loading from "../layout/Loading";
+import Container from "../layout/Container";
 
 function Project() {
-    const { id } = useParams();
-    const [project, setProject] = useState(null);
+  const { id } = useParams();
+  const [project, setProject] = useState(null);
+  const [showProjectForm, setShowProjectForm] = useState(false);
 
-    useEffect(() => {
-        fetch(`http://localhost:5000/projects/${id}`, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        })
-        .then(resp => resp.json())
+  useEffect(() => {
+    setTimeout(() => {
+      fetch(`http://localhost:5000/projects/${id}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((resp) => resp.json())
         .then((data) => {
-            setProject(data);
-            console.log("Dados recebidos:", data);
+          setProject(data);
+          console.log("Dados recebidos:", data);
         })
         .catch((err) => console.error("Erro ao buscar projeto:", err));
-    }, [id]); 
-    return (
-        <div>
-            <h1>Projeto Detalhes</h1>
-            
+    }, 1000);
+  }, [id]);
+
+  function toggleProjectForm() {
+    setShowProjectForm(!showProjectForm);
+  }
+
+  return (
+    <>
+      {project ? (
+        <div className={style.project_details}>
+          <Container customClass="column">
+            <div className={style.details_container}>
+              <h1>Projeto: {project.name}</h1>
+              <button onClick={toggleProjectForm} className={style.btn}>
+                {!showProjectForm ? "Editar Projeto" : "Fechar Projeto"}
+              </button>
+            </div>
+            {!showProjectForm ? (
+                <div className={style.project_info}>
+                <p>
+                  <span>Categoria:</span> {project.category.name}
+                </p>
+
+                <p>
+                  <span>Total de orçamento:</span> {project.budget}
+                </p>
+
+                <p>
+                  <span>Total utilizado:</span> {project.cost}
+                </p>
+              </div>
+            ) : (
+                <div className={style.project_info}>
+                    <p> detalhes do projeto</p>
+                </div>
+            )}
+          </Container>
         </div>
-    );
+      ) : (
+        <Loading />
+      )}
+    </>
+  );
 }
 
 export default Project;
